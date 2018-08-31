640b923005efd5b9dd43a8f0af10842daaa5735c
Dylan Burr <djbn65@mst.edu>
2018-08-31 15:50:56 -0500

    Changed timer style, team picture style, other small things

--- js/timeCounter.js
+++ js/timeCounter.js
@@ -4,14 +4,14 @@ var countDown = function(theSelector, time){
   var theDate = Date.parse(new Date());
   var difference = dTime - theDate;
   var milliseconds = difference % 1000;
-  
+
   function addZero(number){
   	if(number <= 9){
     	number = "0" + number;
     }
     return number;
   }
-  
+
   x = difference / 1000;
   seconds = addZero(parseInt(x % 60));
   x /= 60;
@@ -20,10 +20,10 @@ var countDown = function(theSelector, time){
   hours = addZero(parseInt(x % 24));
   x /= 24;
   days = addZero(parseInt(x));
-  
+
   output += "<span class='days'>" + days + "<small>Days</small></span>";
   output += "<span class='hours'>" + hours + "<small>Hours</small></span>";
-  output += "<span class='minutes'>" + minutes + "<small>Minutes</small></span>";
-  output += "<span class='seconds'>" + seconds + "<small>Seconds</small></span>";
+  output += "<span class='minutes'>" + minutes + "<small>Mins</small></span>";
+  output += "<span class='seconds'>" + seconds + "<small>Secs</small></span>";
   document.querySelector(theSelector).innerHTML = output;
 }
